42ee96881ee0371c3e1300a9a4d1bf6c06d0b92b
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2022-07-21 20:05:01 +0200

    [FEATURE] add support for using KSQL DB stock repository

--- src/main/java/com/piotr/stock/streaming/processor/RetrieveStockProcessor.java
+++ src/main/java/com/piotr/stock/streaming/processor/RetrieveStockProcessor.java
@@ -1,6 +1,7 @@
 package com.piotr.stock.streaming.processor;
 
 import com.piotr.stock.streaming.entity.StockEntity;
+import com.piotr.stock.streaming.mapper.RowMapper;
 import com.piotr.stock.streaming.repository.StockQueryInfo;
 import com.piotr.stock.streaming.repository.StockRepository;
 import java.util.List;
@@ -12,13 +13,20 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class RetrieveStockProcessor {
 
+  private final RowMapper rowMapper;
   private final StockRepository stockRepository;
 
   @Handler
   public List<StockEntity> process(StockQueryInfo stockQueryInfo) {
     if(stockQueryInfo.getQueryParams().isEmpty()) {
-      return stockRepository.findStockLimited();
+      return stockRepository.findStockRowsLimited()
+          .stream()
+          .map(rowMapper::mapRowToStockEntity)
+          .toList();
     }
-    return stockRepository.findStockList(stockQueryInfo);
+    return stockRepository.findStockRows(stockQueryInfo)
+        .stream()
+        .map(rowMapper::mapRowToStockEntity)
+        .toList();
   }
 }
--- src/test/java/com/piotr/stock/streaming/processor/RetrieveStockProcessorTest.java
+++ src/test/java/com/piotr/stock/streaming/processor/RetrieveStockProcessorTest.java
@@ -1,9 +1,10 @@
 package com.piotr.stock.streaming.processor;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 
+import com.piotr.stock.streaming.ksql.KsqlDbDriver;
+import com.piotr.stock.streaming.mapper.RowMapper;
 import com.piotr.stock.streaming.model.ApiRequestParams;
 import com.piotr.stock.streaming.repository.StockQueryInfo;
 import com.piotr.stock.streaming.repository.StockRepository;
@@ -12,30 +13,26 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 @ExtendWith(MockitoExtension.class)
 class RetrieveStockProcessorTest {
 
+  private static final String KSQL_TABLE = "STOCK";
+
   private RetrieveStockProcessor retrieveStockProcessor;
 
-  @InjectMocks
-  private StockRepository stockRepository;
-
   @Mock
-  private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+  private KsqlDbDriver ksqlDbDriver;
 
   @Captor
   private ArgumentCaptor<String> sqlQueryCaptor;
 
   @BeforeEach
   void setUp() {
-    retrieveStockProcessor = new RetrieveStockProcessor(stockRepository);
+    StockRepository stockRepository = new StockRepository(KSQL_TABLE, ksqlDbDriver);
+    retrieveStockProcessor = new RetrieveStockProcessor(new RowMapper(), stockRepository);
   }
 
   @Test
@@ -43,14 +40,13 @@ class RetrieveStockProcessorTest {
     //given
     ApiRequestParams apiRequestParams = new ApiRequestParams("BTC/USD", null, null);
     StockQueryInfo stockQueryInfo = new StockQueryInfo(apiRequestParams);
-    String expectedSql = "SELECT * FROM STOCK WHERE TICKER = :TICKER";
+    String expectedSql = "SELECT * FROM STOCK WHERE TICKER = 'BTC/USD';";
 
     //when
     retrieveStockProcessor.process(stockQueryInfo);
 
     //then
-    verify(namedParameterJdbcTemplate).query(sqlQueryCaptor.capture(), any(SqlParameterSource.class),
-        any(BeanPropertyRowMapper.class));
+    verify(ksqlDbDriver).findByQuery(sqlQueryCaptor.capture());
 
     String actualSql = sqlQueryCaptor.getValue();
     assertThat(actualSql).isEqualTo(expectedSql);
@@ -61,16 +57,15 @@ class RetrieveStockProcessorTest {
     //given
     ApiRequestParams apiRequestParams = new ApiRequestParams();
     StockQueryInfo stockQueryInfo = new StockQueryInfo(apiRequestParams);
-    String expectedSql = "SELECT * FROM STOCK LIMIT 10";
+    String expectedSql = "SELECT * FROM STOCK LIMIT 10;";
 
     //when
     retrieveStockProcessor.process(stockQueryInfo);
 
     //then
-    verify(namedParameterJdbcTemplate).query(sqlQueryCaptor.capture(), any(BeanPropertyRowMapper.class));
+    verify(ksqlDbDriver).findByQuery(sqlQueryCaptor.capture());
 
     String actualSql = sqlQueryCaptor.getValue();
     assertThat(actualSql).isEqualTo(expectedSql);
   }
-
 }
