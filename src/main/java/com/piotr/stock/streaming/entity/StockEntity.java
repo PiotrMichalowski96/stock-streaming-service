6614770e9105922c9bd20036dbe77f2eac2c8150
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2022-07-21 19:46:51 +0200

    [FEATURE] update StockEntity model, mappers and add serializer

--- src/main/java/com/piotr/stock/streaming/entity/StockEntity.java
+++ src/main/java/com/piotr/stock/streaming/entity/StockEntity.java
@@ -1,53 +1,41 @@
 package com.piotr.stock.streaming.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column;
-import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(StockEntity.TABLE_NAME)
 public class StockEntity {
 
-  public static final String TABLE_NAME = "STOCK";
+  public static final String ID = "ID";
   public static final String TICKER_COLUMN = "TICKER";
-  public static final String STOCK_TYPE_COLUMN = "STOCK_TYPE";
+  public static final String STOCK_TYPE_COLUMN = "STOCKTYPE";
   public static final String EXCHANGE_COLUMN = "EXCHANGE";
   public static final String PRICE_COLUMN = "PRICE";
   public static final String CURRENCY_COLUMN = "CURRENCY";
   public static final String VOLUME_COLUMN = "VOLUME";
-  public static final String STOCK_TIMESTAMP_COLUMN = "STOCK_TIMESTAMP";
+  public static final String STOCK_TIMESTAMP_COLUMN = "STOCKTIMESTAMP";
 
-  @Id
   private Long id;
-
-  @Column(StockEntity.TICKER_COLUMN)
   private String ticker;
-
-  @Column(StockEntity.STOCK_TYPE_COLUMN)
   private String stockType;
-
-  @Column(StockEntity.EXCHANGE_COLUMN)
   private String exchange;
-
-  @Column(StockEntity.PRICE_COLUMN)
   private BigDecimal price;
-
-  @Column(StockEntity.CURRENCY_COLUMN)
   private String currency;
-
-  @Column(StockEntity.VOLUME_COLUMN)
-  private BigInteger volume;
-
-  @Column(StockEntity.STOCK_TIMESTAMP_COLUMN)
+  private Long volume;
+  @JsonFormat(pattern ="yyyy-MM-dd HH:mm:ss")
+  @JsonSerialize(using = LocalDateTimeSerializer.class)
+  @JsonDeserialize(using = LocalDateTimeDeserializer.class)
   private LocalDateTime stockTimestamp;
 }
---
+++ src/test/java/com/piotr/stock/streaming/util/TestDataFactoryUtil.java
@@ -0,0 +1,84 @@
+package com.piotr.stock.streaming.util;
+
+import static com.piotr.stock.streaming.entity.StockEntity.CURRENCY_COLUMN;
+import static com.piotr.stock.streaming.entity.StockEntity.EXCHANGE_COLUMN;
+import static com.piotr.stock.streaming.entity.StockEntity.ID;
+import static com.piotr.stock.streaming.entity.StockEntity.PRICE_COLUMN;
+import static com.piotr.stock.streaming.entity.StockEntity.STOCK_TIMESTAMP_COLUMN;
+import static com.piotr.stock.streaming.entity.StockEntity.STOCK_TYPE_COLUMN;
+import static com.piotr.stock.streaming.entity.StockEntity.TICKER_COLUMN;
+import static com.piotr.stock.streaming.entity.StockEntity.VOLUME_COLUMN;
+import static io.confluent.ksql.api.client.util.RowUtil.columnTypesFromStrings;
+
+import com.piotr.stock.streaming.entity.StockEntity;
+import com.piotr.stock.streaming.rest.model.Stock;
+import io.confluent.ksql.api.client.ColumnType;
+import io.confluent.ksql.api.client.Row;
+import io.confluent.ksql.api.client.impl.RowImpl;
+import io.vertx.core.json.JsonArray;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class TestDataFactoryUtil {
+
+  public static Stock createStockDto(OffsetDateTime stockTimestamp) {
+    return new StockBuilder()
+        .withTicker("BTC/USD")
+        .withQuoteType("Digital Currency")
+        .withStockMarket("Binance")
+        .withPrice(BigDecimal.valueOf(43993.0))
+        .withCurrency("US Dollar")
+        .withVolume(72110L)
+        .withStockTimestamp(stockTimestamp)
+        .build();
+  }
+
+  public static StockEntity createStockEntity(Long id, LocalDateTime stockTimestamp) {
+    return StockEntity.builder()
+        .id(id)
+        .ticker("BTC/USD")
+        .stockType("Digital Currency")
+        .exchange("Binance")
+        .price(BigDecimal.valueOf(43993.0))
+        .currency("US Dollar")
+        .volume(72110L)
+        .stockTimestamp(stockTimestamp)
+        .build();
+  }
+
+  public static Row createKsqlDbRow(Long id, String stockTimestamp) {
+
+    List<String> columnNames = List.of(ID,
+        TICKER_COLUMN,
+        STOCK_TYPE_COLUMN,
+        EXCHANGE_COLUMN,
+        PRICE_COLUMN,
+        CURRENCY_COLUMN,
+        VOLUME_COLUMN,
+        STOCK_TIMESTAMP_COLUMN);
+
+    List<ColumnType> columnTypes = columnTypesFromStrings(List.of("BIGINT", "DECIMAL", "STRING"));
+
+    JsonArray jsonArrayValues = new JsonArray(List.of(id,
+        "BTC/USD",
+        "Digital Currency",
+        "Binance",
+        43993.0,
+        "US Dollar",
+        72110L,
+        stockTimestamp));
+
+    Map<String, Integer> columnNameToIndex = IntStream.range(0, columnNames.size())
+        .boxed()
+        .collect(Collectors.toMap(columnNames::get, index -> index + 1));
+
+    return new RowImpl(columnNames, columnTypes, jsonArrayValues, columnNameToIndex);
+  }
+}
